f55a9b027c36cfb81b69273bc8a3e1df5b0af60d
Aleksuk <aleksuk.m@gmail.com>
2015-05-16 22:38:43 +0300

    Fixed error with checking session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < Devise::SessionsController
   respond_to :json
 
   def check_session
-    render if current_user
+    if current_user
       render plain: 'OK', status: 200
     else
       render plain: 'pzdc', status: 401
