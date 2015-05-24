4c4a1397c26fe838427a06802a83997e4e5cb2ff
Aleksuk <aleksuk.m@gmail.com>
2015-05-25 00:03:20 +0300

    Added i18n for errors. BE returns responseText as text of error

--- app/assets/javascripts/app/Views/Registration.js
+++ app/assets/javascripts/app/Views/Registration.js
@@ -6,17 +6,12 @@
             this.prevDefault(e);
 
             app.instances.session.createUser(this.model.toJSON())
-                .done(this.successRegistration.bind(this))
-                .fail(this.failRegistration.bind(this));
+                .done(this.successRegistration.bind(this));
         },
 
         successRegistration: function (userData) {
             app.instances.user.set(userData);
             app.instances.router.navigate('/', { trigger: true });
-        },
-
-        failRegistration: function (e) {
-            console.warn(JSON.parse(e.responseJSON.errors));
         }
 
     });
