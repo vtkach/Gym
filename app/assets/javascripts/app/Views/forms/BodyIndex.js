9fef2e7e278c87b3c2fca02ce3fe96e4afd11537
Victor Tkachenko <stoo@ukr.net>
2015-05-30 00:11:30 +0300

    add table on body index tab

--- app/assets/javascripts/app/Views/forms/BodyIndex.js
+++ app/assets/javascripts/app/Views/forms/BodyIndex.js
@@ -2,11 +2,57 @@
 
     app.views.BodyIndexView = app.helpers.BaseView.extend({
 
+        classification: [{
+            min: 0,
+            max: 18.5
+        }, {
+            min: 18.5,
+            max: 24.9
+        }, {
+            min: 25,
+            max: 29.9
+        }, {
+            min: 30,
+            max: 34.9
+        }, {
+            min: 35,
+            max: 39.9
+        }, {
+            min: 40,
+            max: 200
+        }],
+
+        colors: [
+            'warning',
+            'success',
+            'warning',
+            'danger',
+            'danger',
+            'danger'
+        ],
+
         events: {
             'change [name=weight], [name=height]': 'changeData'
         },
 
-        changeData: function (event) {
+        eachRange: function (range) {
+            var bodyindex = this.model.get('bodyindex');
+
+            return range.min < bodyindex && range.max > bodyindex;
+        },
+
+        coloredTableRow: function () {
+            var neededRow = _.findKey(this.classification, this.eachRange.bind(this));
+
+            this.$('tbody tr').removeAttr('class').eq(neededRow).addClass(this.colors[neededRow]);
+
+        },
+
+        onInit: function () {
+            this.listenTo(this.model, 'change:bodyindex', this.coloredTableRow);
+        },
+
+        changeData: function () {
             this.model.trigger('change:bodyindex');
         }
 
@@ -15,19 +61,32 @@
             weight: {
                 selector: '[name=weight]',
                 converter: function (dir, value, attr, model) {
-                    model.set('bodyindex', model.get('weight') / model.get('height'));
+                    model.set('bodyindex', model.get('weight') / (model.get('height')) * 100);
 
                     return value;
                 }
             },
-            height: {
+            height: [{
+                selector: '[name=height]',
+                converter: function (dir, value) {
+                    var result;
+
+                    if (Backbone.ModelBinder.Constants.ViewToModel === dir) {
+                        result = value / 100;
+                    } else {
+                        result = value * 100;
+                    }
+
+                    return result;
+                }
+            }, {
                 selector: '[name=height]',
                 converter: function (dir, value, attr, model) {
-                    model.set('bodyindex', model.get('weight') / model.get('height'));
+                    model.set('bodyindex', model.get('weight') / (model.get('height')) * 100);
 
                     return value;
                 }
-            },
+            }],
 
             bodyindex: '[name=bodyindex]'
         }
