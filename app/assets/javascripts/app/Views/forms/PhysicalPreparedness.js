f7504497d71f5e5ab680795bcefcaacd35b215db
Victor Tkachenko <stoo@ukr.net>
2015-06-06 01:26:53 +0300

    fix selector

--- app/assets/javascripts/app/Views/forms/PhysicalPreparedness.js
+++ app/assets/javascripts/app/Views/forms/PhysicalPreparedness.js
@@ -6,8 +6,8 @@
             var bindFields = [
                 "pushups",
                 "raising",
-                "jumpblength",
-                "jumpbheight",
+                "jumplength",
+                "jumpheight",
                 "estapheta",
                 "coopertest",
                 "straightlegs",
