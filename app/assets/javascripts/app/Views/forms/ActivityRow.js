29b6f4fd7cf51f266f10af6ebeb097eefced215b
Victor Tkachenko <stoo@ukr.net>
2015-07-10 00:17:29 +0300

    Updated motor activity (doesn't work)

--- app/assets/javascripts/app/Views/forms/ActivityRow.js
+++ app/assets/javascripts/app/Views/forms/ActivityRow.js
@@ -5,18 +5,25 @@
         tagName: 'tr',
 
         events: {
+            'change [name=description]': 'setActivityLevel',
             'click .glyphicon-remove': 'removeRow'
         },
 
         onInit: function () {
             Backbone.Validation.bind(this);
-            this.listenTo(this.model, 'validated:invalid', this.showRowError.bind(this));
-            this.listenTo(this.model, 'validated:valid', this.removeRowError.bind(this));
-            this.listenTo(this.model, 'error', this.showServerError.bind(this));
+            this.listenTo(this.model, 'validated:invalid', this.showRowError.bind(this))
+                .listenTo(this.model, 'validated:valid', this.removeRowError.bind(this))
+                .listenTo(this.model, 'error', this.showServerError.bind(this));
         },
 
         removeRow: function () {
             this.model.destroy();
+        },
+
+        setActivityLevel: function (event) {
+            var $selected = $(event.target).find(':selected');
+
+            this.model.set('activityLevel', $selected.data('activity-level'));
         },
 
         showRowError: function () {
@@ -28,12 +35,36 @@
             this.$el.removeClass('danger');
         },
 
+        beforeBinding: function () {
+            var bindings = Backbone.ModelBinder.createDefaultBindings(this.el, 'name');
+
+            bindings.activityLevel.converter = function (dir, val) {
+                var dict = {
+                    ml: 'Середній рівень',
+                    bl: 'Базовий рівень',
+                    sl: 'Сидячий рівень',
+                    sml: 'Малий рівень',
+                    hl: 'Високий рівень'
+                };
+
+
+                if (dir === Backbone.ModelBinder.Constants.ModelToView) {
+                    return dict[val];
+                }
+
+                return val;
+            };
+
+            this.constructor.bindings = bindings;
+        },
+
         binding: function () {
+            this.beforeBinding();
             this._modelBinder.bind(
                 this.model,
                 this.el,
                 this.constructor.bindings,
-                { modelSetOptions: { validate: false } }
+                {modelSetOptions: {validate: false}}
             );
             this.extendBinding();
         }
